ea660a146cecf0ea40a68cc4d3bdfbbc597b0dfe
selly <yppeu@naver.com>
2024-01-25 13:33:02 +0900

    0125

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 
 $(document).ready(function(){
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 1,
-      centeredSlides: false,
-      slidesPerGroupSkip: 1,
-      grabCursor: true,
-      loop: true,
-      autoplay: {
-        delay: 2000, // 수정: autoplay 속성을 객체로 사용하여 delay 속성으로 지정
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    centeredSlides: false,
+    slidesPerGroup: 4,
+    slidesPerGroupSkip: 0,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false, // 사용자 상호작용 후에도 자동재생 유지
+      stopOnLast: false, // 마지막 슬라이드에서 멈추지 않음
+    },
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 5,
+        slidesPerGroup: 1,
       },
-      keyboard: {
-        enabled: true,
-      },
-      breakpoints: {
-        769: {
-          slidesPerView: 5,
-          slidesPerGroup: 2,
-        },
-      },
-      scrollbar: {
-        el: ".swiper-scrollbar",
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 });
