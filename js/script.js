6af6dcb505c192693f4cf490bdb2bade8b2f2c03
selly <yppeu@naver.com>
2024-01-29 23:31:39 +0900

    0129

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 
 $(document).ready(function(){
   var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
+    slidesPerView: 3,
     centeredSlides: false,
-    slidesPerGroup: 4,
+    slidesPerGroup: 3,
     slidesPerGroupSkip: 0,
     // grabCursor: true,
     loop: true,
     autoplay: {
-      delay: 4000,
+      delay: 2000,
       disableOnInteraction: false, // 사용자 상호작용 후에도 자동재생 유지
       stopOnLast: false, // 마지막 슬라이드에서 멈추지 않음
     },
@@ -17,7 +17,7 @@ $(document).ready(function(){
     },
     breakpoints: {
       769: {
-        slidesPerView: 5,
+        slidesPerView: 6,
         slidesPerGroup: 1,
       },
     },
